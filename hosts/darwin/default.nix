9d49708b41c80a0afc6cafcf146177fb83aa9f39
Ben Suskins <suskinsdevelopment@gmail.com>
2025-02-12 23:08:55 +0000

    Add mac system props

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -39,34 +39,30 @@ let user = "bensuskins"; in
   system = {
     stateVersion = 4;
 
-    defaults = { # Todo fix this
+    defaults = {
       NSGlobalDomain = {
-        AppleShowAllExtensions = true;
-        ApplePressAndHoldEnabled = false;
-
-        KeyRepeat = 2; # Values: 120, 90, 60, 30, 12, 6, 2
-        InitialKeyRepeat = 15; # Values: 120, 94, 68, 35, 25, 15
-
-        "com.apple.mouse.tapBehavior" = 1;
-        "com.apple.sound.beep.volume" = 0.0;
-        "com.apple.sound.beep.feedback" = 0;
+        "com.apple.keyboard.fnState" = false;
       };
 
       dock = {
-        autohide = false;
-        show-recents = false;
-        launchanim = true;
+        autohide = true;
         orientation = "bottom";
-        tilesize = 48;
+        largesize = 66;
+        tilesize = 58;
+        magnification = true;
+        show-recents = false;
       };
 
       finder = {
-        _FXShowPosixPathInTitle = false;
+        AppleShowAllExtensions = true;
+        ShowPathbar = true;
+        AppleShowAllFiles = true;
+        FXPreferredViewStyle = "icnv";
+        FXEnableExtensionChangeWarning = false;
       };
 
       trackpad = {
         Clicking = true;
-        TrackpadThreeFingerDrag = true;
       };
     };
   };
