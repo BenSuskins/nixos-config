ba3dd658b89093afa2a9bde9b8bb56a1f717f0f2
Ben Suskins <suskinsdevelopment@gmail.com>
2025-02-12 23:04:19 +0000

    Add Git alias'

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -62,5 +62,17 @@ let name = "Ben Suskins";
       pull.rebase = true;
       rebase.autoStash = true;
     };
+     aliases = {
+      st = "status"
+      di = "diff"
+      co = "checkout"
+      ci = "commit"
+      br = "branch"
+      sta = "stash"
+      ds = "diff"
+      unstage = "reset"
+      rum = "rebase master@{u}"
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+    }
   };
 }
