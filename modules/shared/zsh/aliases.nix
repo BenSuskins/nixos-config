26fd6b77bb2f41902ca30c41fbe765ca8927ea79
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-03 22:32:56 +0100

    Formatting

--- modules/shared/zsh/aliases.nix
+++ modules/shared/zsh/aliases.nix
@@ -1,15 +1,16 @@
 { hostRole }:
-{
-    # General
-    nixconfig = "code ~/workspace/nixos-config";
-    rebuild = "cd ~/workspace/nixos-config && darwin-rebuild build --flake .#${hostRole}";
-    switch = "cd ~/workspace/nixos-config && sudo darwin-rebuild switch --flake .#${hostRole}";
-    compare = "cd ~/workspace/nixos-config && nix store diff-closures /run/current-system ./result";
 
-    # SSH
-    sshmedia = "ssh -i ~/.ssh/homelab mediaserver@$MEDIA_SERVER_IP";
-    sshdocker = "ssh -i ~/.ssh/homelab docker@$DOCKER_SERVER_IP";
-    sshgame = "ssh -i ~/.ssh/homelab gameserver@$GAME_SERVER_IP";
-    sshmonitor = "ssh -i ~/.ssh/homelab monitor@$MONITOR_SERVER_IP";
-    sshdevelopment = "ssh -i ~/.ssh/homelab development@$DEVELOPMENT_SERVER_IP";
+{
+  # General
+  nixconfig = "code ~/workspace/nixos-config";
+  rebuild = "cd ~/workspace/nixos-config && darwin-rebuild build --flake .#${hostRole}";
+  switch = "cd ~/workspace/nixos-config && sudo darwin-rebuild switch --flake .#${hostRole}";
+  compare = "cd ~/workspace/nixos-config && nix store diff-closures /run/current-system ./result";
+
+  # SSH
+  sshmedia = "ssh -i ~/.ssh/homelab mediaserver@$MEDIA_SERVER_IP";
+  sshdocker = "ssh -i ~/.ssh/homelab docker@$DOCKER_SERVER_IP";
+  sshgame = "ssh -i ~/.ssh/homelab gameserver@$GAME_SERVER_IP";
+  sshmonitor = "ssh -i ~/.ssh/homelab monitor@$MONITOR_SERVER_IP";
+  sshdevelopment = "ssh -i ~/.ssh/homelab development@$DEVELOPMENT_SERVER_IP";
 }
