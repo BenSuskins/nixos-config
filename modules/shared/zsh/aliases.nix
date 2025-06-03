28a88b0e710942a0d745db8fca473f28694c2fd7
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-03 20:39:03 +0100

    Remove init content

---
+++ modules/shared/zsh/aliases.nix
@@ -0,0 +1,14 @@
+{
+    # General
+    nixconfig = "code ~/workspace/nixos-config";
+    rebuild = "cd ~/workspace/nixos-config && darwin-rebuild build --flake .#personal";
+    switch = "cd ~/workspace/nixos-config && sudo darwin-rebuild switch --flake .#personal";
+    compare = "cd ~/workspace/nixos-config && nix store diff-closures /run/current-system ./result";
+
+    # SSH
+    sshmedia = "ssh -i ~/.ssh/homelab mediaserver@$MEDIA_SERVER_IP";
+    sshdocker = "ssh -i ~/.ssh/homelab docker@$DOCKER_SERVER_IP";
+    sshgame = "ssh -i ~/.ssh/homelab gameserver@$GAME_SERVER_IP";
+    sshmonitor = "ssh -i ~/.ssh/homelab monitor@$MONITOR_SERVER_IP";
+    sshdevelopment = "ssh -i ~/.ssh/homelab development@$DEVELOPMENT_SERVER_IP";
+}
