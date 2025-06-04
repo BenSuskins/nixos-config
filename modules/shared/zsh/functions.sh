cf8beb57cf8eccbc7275186245c33ae7c3a6faba
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-04 15:17:51 +0100

    Add more functions

--- modules/shared/zsh/functions.sh
+++ modules/shared/zsh/functions.sh
@@ -9,6 +9,30 @@ pullall() {
       cd "${d%/.git}" || continue
       echo "==> $PWD"
       git pull
+      echo
+    )
+  done
+}
+
+pullmain() {
+  for d in */.git; do
+    (
+      cd "${d%/.git}" || continue
+      echo "==> $PWD"
+      git checkout main
+      git pull
+      echo
+    )
+  done
+}
+
+showbranch() {
+  for d in */.git; do
+    (
+      cd "${d%/.git}" || continue
+      echo "==> $PWD"
+      git branch --show-current
+      echo
     )
   done
 }
