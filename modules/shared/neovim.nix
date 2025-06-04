fb3496bd523138318a1933df56693cc3a7bd0071
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-04 23:26:14 +0100

    Neovim config

--- modules/shared/neovim.nix
+++ modules/shared/neovim.nix
@@ -10,15 +10,25 @@
     plenary-nvim
     telescope-nvim
     nvim-treesitter
-    which-key-nvim
     mini-nvim
   ];
 
   extraConfig = ''
     colorscheme tokyonight
+    set number
+    set cursorline
   '';
  
   extraLuaConfig = ''
     require('mini.statusline').setup()
+    require('mini.starter').setup()
+    require('mini.icons').setup()
+    require('mini.git').setup()
+    require('mini.sessions').setup()
+    require('mini.visits').setup()
+
+    vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>fc', '<cmd>Telescope git_commits<CR>', { noremap = true, silent = true })
   '';
 }
