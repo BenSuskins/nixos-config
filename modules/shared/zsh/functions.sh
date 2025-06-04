712d99b3d2805e22513340d5c8dd6f4abe86f6b1
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-04 15:11:57 +0100

    functions!

--- modules/shared/zsh/functions.sh
+++ modules/shared/zsh/functions.sh
@@ -2,3 +2,13 @@ dgrep() {
   # A recursive, case-insensitive grep that excludes binary files
   grep -iR "$@" * | grep -v "Binary"
 }
+
+pullall() {
+  for d in */.git; do
+    (
+      cd "${d%/.git}" || continue
+      echo "==> $PWD"
+      git pull
+    )
+  done
+}
