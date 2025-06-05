619413a21aed6426ace399e080decb1ac29fae8a
Ben Suskins <suskinsdevelopment@gmail.com>
2025-06-05 20:59:16 +0100

    Go back to plugins

--- modules/shared/zsh.nix
+++ modules/shared/zsh.nix
@@ -4,10 +4,6 @@
   enable = true;
   autocd = false;
 
-  autosuggestion = {
-    enable = true;
-  };
-
   plugins = import ./zsh/plugins.nix;
   shellAliases = import ./zsh/aliases.nix { inherit hostRole; };
   sessionVariables = import ./zsh/variables.nix;
--- modules/shared/zsh/plugins.nix
+++ modules/shared/zsh/plugins.nix
@@ -4,7 +4,23 @@
     file = "pure.zsh";
     src = builtins.fetchGit {
       url = "https://github.com/sindresorhus/pure";
-      rev = "92b8e9057988566b37ff695e70e2e9bbeb7196c8";
+      rev = "5c2158096cd992ad73ae4b42aa43ee618383e092";
+    };
+  }
+  {
+    name = "zsh-syntax-highlighting";
+    file = "zsh-syntax-highlighting.zsh";
+    src = builtins.fetchGit {
+      url = "https://github.com/zsh-users/zsh-syntax-highlighting/";
+      rev = "5eb677bb0fa9a3e60f0eff031dc13926e093df92";
+    };
+  }
+  {
+    name = "zsh-autosuggestions";
+    file = "zsh-autosuggestions.zsh";
+    src = builtins.fetchGit {
+      url = "https://github.com/zsh-users/zsh-autosuggestions";
+      rev = "0e810e5afa27acbd074398eefbe28d13005dbc15";
     };
   }
 ]
