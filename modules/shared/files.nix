d20cf931507b685de6bbe3d6f7b9314c1090acab
Ben Suskins <suskinsdevelopment@gmail.com>
2025-08-20 13:55:58 +0100

    Remove idea

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,11 +1,6 @@
 { hostRole }:
 
 {
-  ".local/bin/idea" = {
-    source = ../../programs/idea/${hostRole}/idea;
-    executable = true;
-  };
-
   ".config/nvim" = {
     source = ../../programs/neovim;
     recursive = true;
