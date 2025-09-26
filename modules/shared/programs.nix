ae72dc8bb1751ec4314ea013a24ecb2e61148207
Ben Suskins <suskinsdevelopment@gmail.com>
2025-09-26 21:24:17 +0100

    Ghostty

--- modules/shared/programs.nix
+++ modules/shared/programs.nix
@@ -14,7 +14,7 @@
   direnv = import ../shared/direnv.nix;
   ghostty = {
     enable = true;
-  }
+  };
   zsh = import ../shared/zsh.nix { inherit hostRole; };
   git = import ../shared/git.nix {
     name = userInfo.name;
