2f0014d2569e9403dbae3f67362c7db707daedc9
Ben Suskins <suskinsdevelopment@gmail.com>
2025-10-26 10:18:39 +0000

    Update git config

--- modules/shared/git.nix
+++ modules/shared/git.nix
@@ -3,37 +3,39 @@
 {
   enable = true;
   ignores = [ "*.swp" ".envrc" ];
-  userName = name;
-  userEmail = email;
+  settings ={
+      aliases = {
+        st = "status";
+        di = "diff";
+        co = "checkout";
+        ci = "commit";
+        br = "branch";
+        sta = "stash";
+        ds = "diff";
+        unstage = "reset";
+        rum = "rebase master@{u}";
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
+      user = {
+        name = name;
+        email = email;
+      };
+      init.defaultBranch = "main";
+      core.autocrlf = "input";
+      pull.rebase = true;
+      pull.ff = "only";
+      rebase.autoStash = true;
+      push.default = "simple";
+      push.autoSetupRemote = true;
+      fetch.prune = true;
+      merge.ff = "only";
+      color.ui = "auto";
+      user.signingkey = "~/.ssh/signing_key";
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+  };
   lfs = {
     enable = true;
-  };
-  extraConfig = {
-    init.defaultBranch = "main";
-    core.autocrlf = "input";
-    pull.rebase = true;
-    pull.ff = "only";
-    rebase.autoStash = true;
-    push.default = "simple";
-    push.autoSetupRemote = true;
-    fetch.prune = true;
-    merge.ff = "only";
-    color.ui = "auto";
-    user.signingkey = "~/.ssh/signing_key";
-    gpg.format = "ssh";
-    commit.gpgsign = true;
-    gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-  };
-  aliases = {
-    st = "status";
-    di = "diff";
-    co = "checkout";
-    ci = "commit";
-    br = "branch";
-    sta = "stash";
-    ds = "diff";
-    unstage = "reset";
-    rum = "rebase master@{u}";
-    lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
   };
 }
