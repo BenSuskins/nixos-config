00460f222db39b899fc0941d3424cf24fe5512e5
Ben Suskins <suskinsdevelopment@gmail.com>
2025-11-26 19:42:38 +0000

    Bump keyrepeat

--- modules/shared/system.nix
+++ modules/shared/system.nix
@@ -10,7 +10,7 @@
         "com.apple.keyboard.fnState" = false;
         "com.apple.mouse.tapBehavior" = 1;
           # 120, 90, 60, 30, 12, 6, 2
-        KeyRepeat = 6;
+        KeyRepeat = 2;
 
         # 120, 94, 68, 35, 25, 15
         InitialKeyRepeat = 15;
