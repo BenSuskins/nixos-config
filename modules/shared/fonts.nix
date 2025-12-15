e5dfa43b4a92094102a634d65799fe92c1feca25
Ben Suskins <suskinsdevelopment@gmail.com>
2025-12-15 21:49:49 +0000

    updates

--- modules/shared/fonts.nix
+++ modules/shared/fonts.nix
@@ -1,10 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  # Only include fonts that exist for the current system
+  availableFonts = lib.filter (f: f != null) [
+    pkgs.nerd-fonts.fira-code
+    pkgs.nerd-fonts.jetbrains-mono
+  ];
+in
 {
-  # fonts = {
-  #   packages = with pkgs; [
-  #     nerd-fonts.fira-code
-  #     nerd-fonts.jetbrains-mono
-  #   ];
-  # };
+  fonts = {
+    enable = true;  # make sure fonts module is enabled
+    packages = availableFonts;
+  };
 }
